7618cfd1f9ebbbdcdae9ec32ceab251472127cb9
MohmadFaizan <faizakodrawala@gmail.com>
2026-05-24 13:41:45 +0530

    updated executorservice as dependency

--- src/main/java/com/natwest/tc/service/Intersection.java
+++ src/main/java/com/natwest/tc/service/Intersection.java
@@ -1,26 +1,25 @@
 package com.natwest.tc.service;
 
 import com.natwest.tc.constants.Direction;
-import com.natwest.tc.constants.Signal;
+import com.natwest.tc.model.BasicTrafficLight;
 import com.natwest.tc.model.TrafficLight;
+import jakarta.annotation.Nonnull;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Service
 public class Intersection {
-    private final ConcurrentHashMap<Direction, TrafficLight> machine = new ConcurrentHashMap<>();
-    private final ExecutorService executor;
+    private final ConcurrentHashMap<Direction, TrafficLight> controller = new ConcurrentHashMap<>();
+    private final ExecutorService machine;
 
-    public Intersection() {
+    public Intersection(@Nonnull final ExecutorService executor) {
         for (Direction d : Direction.values()) {
-            machine.put(d, new TrafficLight(d, Signal.RED));
+            controller.put(d, new BasicTrafficLight(d));
         }
 
-        this.executor = Executors.newFixedThreadPool(1);
+        this.machine = executor;
     }
 
-    
 }
