06b66bae2ae5e55ba5b080ce02614981dc3117bd
MohmadFaizan <faizakodrawala@gmail.com>
2026-05-24 13:41:06 +0530

    Updated traffic light obj creation

--- src/main/java/com/natwest/tc/model/TrafficLight.java
+++ src/main/java/com/natwest/tc/model/TrafficLight.java
@@ -5,11 +5,11 @@ import com.natwest.tc.constants.Signal;
 
 import java.io.Serializable;
 
-public class TrafficLight implements Serializable {
+public abstract class TrafficLight implements Serializable {
     private final Direction direction;
     private final Signal state;
 
-    public TrafficLight(final Direction direction, final Signal state) {
+    TrafficLight(final Direction direction, final Signal state) {
         this.direction = direction;
         this.state = state;
     }
@@ -20,5 +20,17 @@ public class TrafficLight implements Serializable {
 
     public Signal getState() {
         return this.state;
+    }
+
+    public final boolean isGreen() {
+        return getState() == Signal.GREEN;
+    }
+
+    public final boolean isYellow() {
+        return getState() == Signal.YELLOW;
+    }
+
+    public final boolean isRed() {
+        return !isGreen() && !isYellow();
     }
 }
