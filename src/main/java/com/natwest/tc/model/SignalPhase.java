b988cac4a34d6477ff3cc3a8f3c21b1c2dfe082d
MohmadFaizan <faizakodrawala@gmail.com>
2026-05-26 17:29:35 +0530

    Revert "updated code to create default phase"
    This reverts commit fc0743da5284b799370ece18941b5c63f7538127.

--- src/main/java/com/natwest/tc/model/SignalPhase.java
+++ src/main/java/com/natwest/tc/model/SignalPhase.java
@@ -2,22 +2,21 @@ package com.natwest.tc.model;
 
 import com.natwest.tc.constants.Constants;
 import com.natwest.tc.constants.Direction;
-import com.natwest.tc.constants.Phase;
 import org.springframework.util.Assert;
 
 import java.util.Set;
 
 public class SignalPhase {
-    private final Phase phase;
+    private final Set<Direction> directions;
     private int greenDelayInSec = Constants.DEFAULT_GREEN_DELAY;
     private int yellowDelayInSec = Constants.DEFAULT_YELLOW_DELAY;
 
-    public SignalPhase(final Phase phase) {
-        this.phase = phase;
+    public SignalPhase(Set<Direction> directions) {
+        this.directions = directions;
     }
 
     public Set<Direction> getDirections() {
-        return this.phase.getDirections();
+        return directions;
     }
 
     public int getGreenDelayInSec() {
--- src/main/java/com/natwest/tc/service/Intersection.java
+++ src/main/java/com/natwest/tc/service/Intersection.java
@@ -1,7 +1,6 @@
 package com.natwest.tc.service;
 
 import com.natwest.tc.constants.Direction;
-import com.natwest.tc.constants.Phase;
 import com.natwest.tc.constants.Signal;
 import com.natwest.tc.exceptions.InvalidStateException;
 import com.natwest.tc.model.SignalPhase;
@@ -31,11 +30,6 @@ public class Intersection implements Runnable {
         for (Direction d : Direction.values()) {
             signals.put(d, new TrafficLight(d));
         }
-
-        final SignalPhase nsPhase = new SignalPhase(Phase.NS_PHASE);
-        final SignalPhase ewPhase = new SignalPhase(Phase.EW_PHASE);
-
-        this.phases = List.of(nsPhase, ewPhase);
     }
 
     @Override
@@ -66,8 +60,17 @@ public class Intersection implements Runnable {
         return this.id;
     }
 
-    public void updateSequence(final Direction direction, final Signal state) {
+    public void updateSequence(final List<SignalPhase> phases) {
+        if (CollectionUtils.isEmpty(phases)) {
+            return;
+        }
 
+        try {
+            lock.lock();
+            this.phases = Collections.unmodifiableList(phases);
+        } finally {
+            lock.unlock();
+        }
     }
 
     private void updateAllRed() {
