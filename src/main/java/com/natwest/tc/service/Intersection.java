01110e4d13faa49ee81db729b1799c9a4334c771
MohmadFaizan <faizakodrawala@gmail.com>
2026-05-27 06:39:47 +0530

    Changes to handle signal cycle

--- src/main/java/com/natwest/tc/service/Intersection.java
+++ src/main/java/com/natwest/tc/service/Intersection.java
@@ -4,6 +4,7 @@ import com.natwest.tc.constants.Direction;
 import com.natwest.tc.constants.Signal;
 import com.natwest.tc.exceptions.ConflictStateUpdateException;
 import com.natwest.tc.exceptions.InvalidStateException;
+import com.natwest.tc.model.SignalCycle;
 import com.natwest.tc.model.SignalPhase;
 import com.natwest.tc.model.TrafficLight;
 
@@ -17,13 +18,13 @@ public class Intersection implements Runnable {
     private final String id;
     private final Map<Direction, TrafficLight> signals = new EnumMap<>(Direction.class);
     private final List<SignalPhase> phases = new ArrayList<>(2);
+    private final SignalCycle cycle;
 
     private final AtomicInteger currentPhaseIndex = new AtomicInteger(0);
     private final AtomicBoolean paused = new AtomicBoolean(false);
     private final AtomicBoolean running = new AtomicBoolean(true);
 
     private final ReentrantLock lock = new ReentrantLock();
-    private Thread controlThread;
 
     public Intersection() {
         this.id = UUID.randomUUID().toString();
@@ -34,32 +35,29 @@ public class Intersection implements Runnable {
 
         this.phases.add(new SignalPhase(Set.of(Direction.NORTH, Direction.SOUTH)));
         this.phases.add(new SignalPhase(Set.of(Direction.EAST, Direction.WEST)));
+
+        this.cycle = new SignalCycle(Signal.values().length);
     }
 
     @Override
     public void run() {
-        controlThread = Thread.currentThread();
-        while (this.running.get() && !controlThread.isInterrupted()) {
-            final SignalPhase phase = this.phases.get(this.currentPhaseIndex.get());
+        while (this.running.get()) {
+            cycle.next();
 
-            delay(2);
+            final SignalPhase phase = getActivePhase();
 
             updatePhase(phase);
+
+            if (cycle.isCompleted()) {
+                updateCurrentPhaseIndex();
+            } else {
+                delay(5);
+            }
         }
     }
 
     private SignalPhase getActivePhase() {
-        final Direction activeDirection = getState().entrySet().stream()
-                .filter(t -> t.getValue() != Signal.RED)
-                .map(Map.Entry::getKey)
-                .findFirst()
-                .orElse(null);
-
-        if (activeDirection == null) {
-            return null;
-        }
-
-        return getPhaseByDirection(activeDirection);
+        return this.phases.get(this.currentPhaseIndex.get());
     }
 
     private void updateCurrentPhaseIndex() {
@@ -73,19 +71,12 @@ public class Intersection implements Runnable {
     public void updateSignal(final Direction targetDirection, final Signal targetColor) {
         final SignalPhase activePhase = getActivePhase();
 
-        final SignalPhase targetPhase = getPhaseByDirection(targetDirection);
-
         lock.lock();
         try {
-            if (activePhase == null && targetColor == Signal.RED) {
-                // All Directions RED, no update required
-                return;
-            }
-
             validateConflict(activePhase, targetDirection, targetColor);
 
             this.signals.values().stream()
-                    .filter(t -> targetPhase.getDirections().contains(t.getDirection()))
+                    .filter(t -> activePhase.getDirections().contains(t.getDirection()))
                     .forEach(t -> t.setState(TrafficLightState.getState(targetColor)));
         } finally {
             this.lock.unlock();
@@ -94,21 +85,8 @@ public class Intersection implements Runnable {
 
     private void validateConflict(final SignalPhase activePhase, final Direction targetDirection,
                                   final Signal targetColor) {
-        if (activePhase != null && !activePhase.getDirections().contains(targetDirection)
-                && Signal.RED != targetColor) {
+        if (!activePhase.getDirections().contains(targetDirection) && Signal.RED != targetColor) {
             throw new ConflictStateUpdateException("Direction Conflict Detected");
-        }
-    }
-
-    private SignalPhase getPhaseByDirection(final Direction direction) {
-        lock.lock();
-        try {
-            return this.phases.stream()
-                    .filter(p -> p.getDirections().contains(direction))
-                    .findFirst()
-                    .get();
-        } finally {
-            lock.unlock();
         }
     }
 
@@ -129,21 +107,13 @@ public class Intersection implements Runnable {
     private void updatePhase(final SignalPhase phase) {
         awaitIfPaused();
 
-        final boolean[] isYellow = {false};
-
         lock.lock();
         try {
             signals.values().stream()
                     .filter(light -> phase.getDirections().contains(light.getDirection()))
-                    .peek(light -> isYellow[0] = (light.getState().getSignal() == Signal.YELLOW))
                     .forEach(light -> light.getState().next(light));
-
         } finally {
             lock.unlock();
-        }
-
-        if (isYellow[0]) {
-            updateCurrentPhaseIndex();
         }
     }
 
@@ -172,9 +142,5 @@ public class Intersection implements Runnable {
         }
 
         this.running.set(false);
-        if (this.controlThread != null) {
-            lock.lock();
-            try { this.controlThread.interrupt(); } finally { lock.unlock(); }
-        }
     }
 }
